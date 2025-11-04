1ecadd5a8309f44f0d5f01a9849329b105bc416b
Your Name <your.email@example.com>
2025-11-05 02:40:11 +0530

    html in main.jsx and why js file not running

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -3,8 +3,20 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+function MyApp() {
+  return (
+  <div>
+    <h1>Custom App</h1>
+  </div>
+  )
+}
+
+const anotherElement = (
+  <div>
+    <a href="https://google.com" target='_blank'>Visit Google</a>
+  </div>
+)
+
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    anotherElement
 )
