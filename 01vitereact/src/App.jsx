e73eb1b9c3e5549f2af7be0f15ec580c257dcbd2
Your Name <your.email@example.com>
2025-11-05 01:05:31 +0530

    feat: update App component to include headings

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -2,7 +2,11 @@ import Chai from './chai.jsx'
 function App() {
 
   return (
+    <>
       <Chai />
+      <h1>Chai aur react | shrey</h1>
+      <h2>This is a subheading</h2>
+      </>
   );
 }
 
